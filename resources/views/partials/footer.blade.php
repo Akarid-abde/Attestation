a1b965ceb22d9baffa1d88280d9f740839855572
Akarid-abde <abderrahim.akarid@gmail.com>
2023-05-05 12:04:01 +0100

    test changer titre

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -62,29 +62,7 @@
             <a href="#!" class="text-reset">Contact</a>
           </p>
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <!-- <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-      
-          <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Pricing</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Orders</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Help</a>
-          </p>
-        </div> -->
-        <!-- Grid column -->
-
+        
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
